09561f8ac5d8755adfbf5722f20936edadcde105
Tikhon Zaikin <th@thevhs.club>
2023-11-18 01:26:31 +0100

    make map reduce abstract

--- etl_pipes/pipes/map_reduce.py
+++ etl_pipes/pipes/map_reduce.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from collections.abc import Sequence
 from dataclasses import dataclass
 from typing import Any
@@ -6,23 +7,21 @@ from etl_pipes.pipes.base_pipe import Pipe
 
 
 @dataclass
-class MapReduce(Pipe):
+class MapReduce(Pipe, ABC):
     async def __call__(self, *args: Any, **kwargs: Any) -> Any:
         chunks = await self.split(*args, **kwargs)
         mapped_chunks = await self.map(chunks)
         reduced_result = await self.reduce(mapped_chunks)
         return reduced_result
 
+    @abstractmethod
     async def split(self, *args: Any, **kwargs: Any) -> Sequence[Any]:
-        return [*args]
+        ...
 
+    @abstractmethod
     async def map(self, chunks: Sequence[Any]) -> Sequence[Any]:
-        def log_chunk(idx: int, c_: Any) -> Any:
-            print(f"chunk {idx}: {c_}")
-            return c_
+        ...
 
-        results = [log_chunk(i, chunk) for i, chunk in enumerate(chunks)]
-        return results
-
+    @abstractmethod
     async def reduce(self, mapped_chunks: Sequence[Any]) -> Any:
-        return tuple(mapped_chunks)
+        ...
--- tests/pipes/map_reduce_test.py
+++ tests/pipes/map_reduce_test.py
@@ -1,4 +1,6 @@
+from collections.abc import Sequence
 from dataclasses import dataclass
+from typing import Any
 
 import pytest
 
@@ -8,12 +10,28 @@ from etl_pipes.pipes.map_reduce import MapReduce
 @pytest.mark.asyncio
 async def test_if_map_reduce_pipe_works() -> None:
     @dataclass
+    class SomeMapReduce(MapReduce):
+        async def split(self, *args: Any) -> Sequence[Any]:
+            return [*args]
+
+        async def map(self, chunks: Sequence[Any]) -> Sequence[Any]:
+            def log_chunk(idx: int, c_: Any) -> Any:
+                print(f"chunk {idx}: {c_}")
+                return c_
+
+            results = [log_chunk(i, chunk) for i, chunk in enumerate(chunks)]
+            return results
+
+        async def reduce(self, mapped_chunks: Sequence[Any]) -> Any:
+            return tuple(mapped_chunks)
+
+    @dataclass
     class SomeObject:
         a: int = 0
         b: str = ""
         c: float = 0.0
 
-    map_reduce = MapReduce()
+    map_reduce = SomeMapReduce()
     chunks = (1, "string", 2.0, 3, "another string", SomeObject())
     result = await map_reduce(*chunks)
 
