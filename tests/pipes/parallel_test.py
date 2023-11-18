5d5c65cfea1bda8b85ca65a8f85085b467a0f59b
Tikhon Zaikin <th@thevhs.club>
2023-11-18 01:05:16 +0100

    fix parallel test

--- tests/pipes/parallel_test.py
+++ tests/pipes/parallel_test.py
@@ -4,50 +4,47 @@ from pathlib import Path
 
 import pytest
 
-from etl_pipes.pipes.base_pipe import Pipe
+from etl_pipes.pipes.base_pipe import as_pipe
 from etl_pipes.pipes.parallel import Parallel
 
 
 @pytest.mark.asyncio
 async def test_parallel_log_to_console_and_log_to_file() -> None:
-    class LogToConsolePipe(Pipe):
-        async def __call__(self, data: str) -> int:
-            await asyncio.sleep(0.5)
-            print(data)
-            return 1
+    @as_pipe
+    async def log_to_console(data: str) -> int:
+        await asyncio.sleep(0.5)
+        print(data)
+        return 1
 
-    test_file = Path("/tmp/pipe-test.txt")
-    if test_file.exists():
-        test_file.unlink()
+    @as_pipe
+    async def log_to_file(data: str) -> Path:
+        test_file = Path("/tmp/pipe-test.txt")
+        if test_file.exists():
+            test_file.unlink()
 
-    class LogToFilePipe(Pipe):
-        async def __call__(self, data: str) -> str:
-            with test_file.open("a") as f:
-                f.write(data)
-            await asyncio.sleep(0.5)
-            return "file"
+        with test_file.open("a") as f:
+            f.write(data)
+        await asyncio.sleep(0.5)
+        return test_file
 
-    log_to_console_pipe = LogToConsolePipe()
-    log_to_file_pipe = LogToFilePipe()
-
-    start_time_ms = int(time.time() * 1000)
     parallel = Parallel(
         [
-            log_to_console_pipe,
-            log_to_file_pipe,
+            log_to_console,
+            log_to_file,
         ],
         broadcast=True,
     )
 
-    results = await parallel("test")
+    start_time_ms = int(time.time() * 1000)
 
-    assert results == (1, "file")
+    exit_code, log_path = await parallel("test")
 
     end_time_ms = int(time.time() * 1000)
-
     limit = 650
     diff = end_time_ms - start_time_ms
     assert diff < limit
 
-    assert test_file.exists()
-    assert test_file.read_text() == "test"
+    assert exit_code == 1
+    assert log_path == Path("/tmp/pipe-test.txt")
+    assert log_path.exists()
+    assert log_path.read_text() == "test"
